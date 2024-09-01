6593ca2a3f2fa3273e0ad7e4bb9681a73ab26a27
lukepham-holmesglen <luke.pham@holmesglen.edu.au>
2024-09-01 15:13:34 +1000

    Replacing controllers with hands and adding hand assets

---
+++ HG_Liminal_blank_template-main/HG_Liminal_blank_template-main/Assets/Scripts/AnimateHandOnInput.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateHandOnInput : MonoBehaviour
+{
+    public string triggerInputName = "Fire1";  // Default to left mouse button or controller trigger
+    public string gripInputName = "Fire2";     // Default to right mouse button or controller grip
+    public Animator handAnimator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // For trigger animation, use the specified input name
+        float triggerValue = Input.GetAxis(triggerInputName);
+        handAnimator.SetFloat("Trigger", triggerValue);
+
+        // For grip animation, use the specified input name
+        float gripValue = Input.GetAxis(gripInputName);
+        handAnimator.SetFloat("Grip", gripValue);
+    }
+}
